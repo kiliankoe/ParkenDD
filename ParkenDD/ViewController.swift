39b68c8904f34e25e0f31671a016d1bcba24fc6b
Kilian Koeltzsch <me@kilian.io>
2015-01-18 11:01:39 +0100

    Initial Commit

---
+++ ParkenDD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParkenDD
+//
+//  Created by Kilian Koeltzsch on 18/01/15.
+//  Copyright (c) 2015 Kilian Koeltzsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
