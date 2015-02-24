b7205d5c8d3305882122206653e32fa1724592e1
Kilian Koeltzsch <me@kilian.io>
2015-02-24 11:57:47 +0100

    App now autoupdates when woken up from background
    See #21

--- ParkenDD/AppDelegate.swift
+++ ParkenDD/AppDelegate.swift
@@ -13,6 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
 
+	var inBackground = false
+
 	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 		// Override point for customization after application launch.
 
@@ -34,6 +36,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	func applicationDidEnterBackground(application: UIApplication) {
 		// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
 		// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+
+		inBackground = true
 	}
 
 	func applicationWillEnterForeground(application: UIApplication) {
@@ -42,6 +46,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func applicationDidBecomeActive(application: UIApplication) {
 		// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+
+		if inBackground {
+			// FIXME: Going through childViewControllers like this feels unbelievably prone to errors...
+			let mainVC = self.window?.rootViewController?.childViewControllers[0] as! ViewController
+			mainVC.updateData()
+			inBackground = false
+		}
 	}
 
 	func applicationWillTerminate(application: UIApplication) {
