930a13b28f460c187ddb8a3bd903d68ef59f3288
Kilian Koeltzsch <me@kilian.io>
2015-04-08 01:43:38 +0200

    Create Snapfile and snapshot.js for fastlane snapshot
    Kinda makes me regret each and every single minute I've spent taking
    screenshots...

---
+++ snapshot.js
@@ -0,0 +1,15 @@
+#import "SnapshotHelper.js"
+
+var target = UIATarget.localTarget();
+var app = target.frontMostApp();
+var window = app.mainWindow();
+
+target.delay(3)
+captureLocalizedScreenshot('0-lotlist')
+target.frontMostApp().navigationBar().leftButton().tap();
+target.delay(3)
+captureLocalizedScreenshot('0-settings')
+target.frontMostApp().navigationBar().leftButton().tap();
+target.frontMostApp().mainWindow().tableViews()[0].tapWithOptions({tapOffset:{x:0.41, y:0.45}});
+target.delay(3)
+captureLocalizedScreenshot('0-schießgasse')
