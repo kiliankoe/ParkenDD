b7c4571771e6b39464375c7d3e9bb29363f895dc
Kilian Koeltzsch <me@kilian.io>
2017-01-11 20:11:31 +0100

    respect show experimental cities setting

--- ParkenDD/SettingsView/CitySelectionTVC.swift
+++ ParkenDD/SettingsView/CitySelectionTVC.swift
@@ -19,7 +19,9 @@ class CitySelectionTVC: UITableViewController {
         ParkKit().fetchCities(onFailure: { error in
             print(error)
         }) { [weak self] response in
-            self?.availableCities = response.cities
+
+            let showExperimental = UserDefaults.standard.bool(forKey: Defaults.showExperimentalCities)
+            self?.availableCities = showExperimental ? response.cities : response.cities.filter { $0.hasActiveSupport }
 
             OperationQueue.main.addOperation {
                 self?.tableView.reloadData()
