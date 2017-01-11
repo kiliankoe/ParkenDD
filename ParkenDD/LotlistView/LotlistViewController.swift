9adb1c2d4fc9076fe7943f4b4e1048f66bf958e3
Kilian Koeltzsch <me@kilian.io>
2017-01-11 23:08:32 +0100

    migrate some of the sortLots functionality

--- ParkenDD/LotlistView/LotlistViewController.swift
+++ ParkenDD/LotlistView/LotlistViewController.swift
@@ -26,7 +26,7 @@ class LotlistViewController: UITableViewController, CLLocationManagerDelegate, U
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
-        tableView.dataSource = LotListDataSource()
+        tableView.dataSource = LotlistDataSource()
 
 		locationManager.delegate = self
 
@@ -110,7 +110,7 @@ class LotlistViewController: UITableViewController, CLLocationManagerDelegate, U
         }) { [weak self] response in
             self?.stopRefreshUI()
             self?.showOutdatedDataWarning(lastUpdated: response.lastUpdated, lastDownloaded: response.lastDownloaded)
-            (self?.tableView.dataSource as? LotlistDataSource)?.parkingLots = response.lots
+            (self?.tableView.dataSource as? LotlistDataSource)?.set(lots: response.lots)
         }
 	}
 
@@ -158,50 +158,6 @@ class LotlistViewController: UITableViewController, CLLocationManagerDelegate, U
 		} else {
 			let selectedCity = UserDefaults.standard.string(forKey: Defaults.selectedCityName)
 			titleButton.setTitle(selectedCity, for: UIControlState())
-		}
-	}
-
-	/**
-	Sort the parkingslots array based on what is currently saved for SortingType in UserDefaults.
-	*/
-	func sortLots() {
-		guard let sortingType = UserDefaults.standard.string(forKey: Defaults.sortingType) else { return }
-		switch sortingType {
-		case Sorting.distance:
-			parkinglots.sort {
-				if let currentUserLocation = locationManager.location,
-                    let dist1 = $0.distance(from: currentUserLocation),
-                    let dist2 = $1.distance(from: currentUserLocation) {
-					return dist1 < dist2
-				}
-				return $0.name < $1.name
-			}
-		case Sorting.alphabetical:
-            parkinglots.sort { $0.name < $1.name }
-		case Sorting.free:
-            parkinglots.sort { $0.freeRegardingClosed > $1.freeRegardingClosed }
-		case Sorting.euclid:
-            self.parkinglots.sort {
-                guard let currentUserLocation = locationManager.location else { return $0.free > $1.free }
-                // TODO: Also check if state is either open or unknown, others should not be sorted
-                if $0.total != 0 && $1.total != 0 {
-                    let occ1 = Double($0.total - $0.free) / Double($0.total)
-                    let occ2 = Double($1.total - $1.free) / Double($1.total)
-
-                    // This factor gives a penalty for very crowded parking spaces
-                    // so they are ranked down the list, even if they are very close
-                    let smoothingfactor1 = 1.0 / Double(2.0*(1.0-occ1))
-                    let smoothingfactor2 = 1.0 / Double(2.0*(1.0-occ2))
-
-                    let sqrt1 = sqrt(pow($0.distance(from: currentUserLocation) ?? 0, 2.0) + smoothingfactor1 * pow(Double(occ1*1000), 2.0))
-                    let sqrt2 = sqrt(pow($1.distance(from: currentUserLocation) ?? 0, 2.0) + smoothingfactor2 * pow(Double(occ2*1000), 2.0))
-                    
-                    return sqrt1 < sqrt2
-                }
-                return $0.free > $1.free
-            }
-		default:
-			parkinglots = defaultSortedParkinglots
 		}
 	}
 
