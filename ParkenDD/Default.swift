893fc1ac5049bd485378b5778d935d1dcef04013
Kilian Koeltzsch <me@kilian.io>
2017-01-12 01:07:10 +0100

    Move UserDefaults extension to Default.swift

--- ParkenDD/Default.swift
+++ ParkenDD/Default.swift
@@ -31,3 +31,30 @@ enum Default: String {
         ]
     }
 }
+
+extension UserDefaults {
+    static func bool(for key: Default) -> Bool {
+        return standard.value(forKey: key.rawValue) as? Bool ?? false
+    }
+
+    static func string(for key: Default) -> String? {
+        return standard.value(forKey: key.rawValue) as? String
+    }
+
+    static func array(for key: Default) -> [Any]? {
+        return standard.value(forKey: key.rawValue) as? [Any]
+    }
+
+
+    static func set(value: Any, forKey key: Default) {
+        standard.set(value, forKey: key.rawValue)
+        standard.synchronize()
+    }
+
+
+    static func register(_ defaults: [Default: Any]) {
+        for (key, val) in defaults {
+            standard.register(defaults: [key.rawValue: val])
+        }
+    }
+}
